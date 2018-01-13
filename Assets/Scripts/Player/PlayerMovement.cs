15e6e47962169f5a20ba3d3d29d195cd3a9f2696
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-13 20:55:22 -0200

    Implemented basic player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    Rigidbody2D rigidbody2d;
+
+    void Awake() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        rigidbody2d.velocity = CalculateMovementSpeed() * CalculateMovementDirection();
+    }
+
+    Vector2 CalculateMovementDirection() {
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (direction != Vector2.zero)
+            direction = direction.normalized;
+        return direction;
+    }
+
+    float CalculateMovementSpeed() {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            return speed / 4f;
+        return speed;
+    }
+}
