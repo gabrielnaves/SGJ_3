6b95aee6053d423c53642ee3064c519f29ec8538
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-13 20:42:03 -0200

    Finally starting scripting. Created a grid snapping script

---
+++ Assets/Scripts/GridSnapping.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSnapping : MonoBehaviour {
+
+    public float pixelsPerUnit = 32f;
+
+    void LateUpdate() {
+        var position = transform.position;
+        position.x = Mathf.Round(position.x * pixelsPerUnit);
+        position.y = Mathf.Round(position.y * pixelsPerUnit);
+        position /= pixelsPerUnit;
+        transform.position = position;
+    }
+}
