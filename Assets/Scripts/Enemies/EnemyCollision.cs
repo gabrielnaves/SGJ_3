2ff456be335db05bddd77b2e0453525f161952b1
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-14 11:30:41 -0200

    Implemented Dad's basic path, added lose on collision

---
+++ Assets/Scripts/Enemies/EnemyCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player") {
+            Overmind.instance.LoseGame();
+        }
+    }
+}
