c2f2b107e9fe8d6e62423cb6b2ecc56836d9d8fe
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-14 16:32:36 -0200

    Fixing ortographic size script for build

--- Assets/Scripts/Camera/CameraOrtoSize.cs
+++ Assets/Scripts/Camera/CameraOrtoSize.cs
@@ -6,9 +6,10 @@ public class CameraOrtoSize : MonoBehaviour {
 
     public float pixelsPerUnit = 32f;
 
-    void Start() {
+    void LateUpdate() {
         float height = Screen.height;
         float ortoSize = height / pixelsPerUnit / 4f;
+        if (ortoSize < 6f) ortoSize *= 2f;
         Camera.main.orthographicSize = ortoSize;
     }
 }
